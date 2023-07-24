99bebec4728d0b066c41c4b9a665b8e21a2cbcff
JDiaz <jdiaz_007@outlook.com>
2023-07-24 09:22:04 -0700

    demo commit

--- public/app.js
+++ public/app.js
@@ -1,21 +1,20 @@
 // get user's data
 // get user's coordinates
-function getCoords(){
-  return new Promise ((resolve, reject) => {
+async function getCoords(){
+  pos = await new Promise ((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject)
   })
+  return [pos.coords.latitude, pos.coords.longitude]
 } 
-console.log(getCoords());
+// console.log(getCoords());
 
 // get user's time
 function userTime() {
     const now = new Date()
     console.log(now)
-    return(now.getHours())
+    return now.getHours()
 }
-userTime()
-console.log('Now let us take a look at our new work!')
-console.log(userTime())
+// console.log(userTime())
 
 
 // helper functions
@@ -32,7 +31,7 @@ function getMealTime(){
     // return tod > 20 ? 'late-night snack' : tod > 16 ? 'dinner' : tod > 11 ? 'lunch' : 'breakfast' // <--- this is an example of a ternary
 }
     
-  console.log(getMealTime())                   
+// console.log(getMealTime())                   
 
 
 
@@ -44,12 +43,29 @@ function buildAd(){
   const mealTime = getMealTime()
   let content = document.querySelector('.ad1')
   let inner = document.createElement('p')
-  inner.innerHTML = We've got the best <span>${mealTime}</span> in town
+  inner.innerHTML = "We've got the best <span>${mealTime}</span> in town"
   content.append(inner)
 }
 buildAd1()
+
 // build ad 2
+// Build Ad 2                                                             
+function buildAd2(coordinates){
+  const coords = coordinates
+  const href = `https://www.google.com/maps/search/coffee/@${coords[0]},${coords[1]},15z/`
+  let content = document.querySelector('.ad2')
+  let inner = document.createElement('p')
+  inner.innerHTML = `It's time to try our coffee! <span><a href="${href}" target="_blank">We're this close!</a></span>`
+  content.append(inner)
+}
+
+// console.log(buildAd2(getCoords()))
 
 
 // event listeners
 // on load, build ads
+window.onload = async () => {
+  buildAd1()
+  const coords = await getCoords()
+  buildAd2(coords)
+}
